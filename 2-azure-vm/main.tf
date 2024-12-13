60473f7a7b8c426c5626993f63346d454dd882b5
Pradeep <pradeep.kumar44@gmail.com>
2024-12-13 14:27:14 +0530

    Update main.tf

--- 2-azure-vm/main.tf
+++ 2-azure-vm/main.tf
@@ -2,11 +2,26 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.13.0"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "azurerm" {
-  # Configuration options
+  features {
+    
+  }
+}
+
+# make use of the exisiting resource group
+data "azurerm_resource_group" "existing"{
+    name = "pradeep"
+}
+
+# virtual network
+resource "azurevm_virtual_network" "vnet"{
+    name = "test-vm-net"
+    address_space = ["10.0.0.0/16"]
+    location = "southindia"
+    resource_group_name = data.azurerm_resource_group.exisiting.name
 }
