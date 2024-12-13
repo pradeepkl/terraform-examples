7af75f31e84f056b3584c719decdebefe1756d4e
Pradeep <pradeep.kumar44@gmail.com>
2024-12-13 09:39:41 +0000

    dfdsf

--- 3-azure-vm/main.tf
+++ 3-azure-vm/main.tf
@@ -2,11 +2,19 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+}
+
+# make use of the exisiting resource group
+resource "azure_virtual_network" "name" {
+  name = "test-vnet"
+  address_space = "[10.0.0.16]"
+  location = "southindia"
+  resource_group_name = "pradeep"
 }
