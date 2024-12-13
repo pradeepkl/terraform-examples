c8eee4773a731aa5d7861dd888bf2e9cf1a81a03
Pradeep <pradeep.kumar44@gmail.com>
2024-12-13 10:10:15 +0000

    test

--- 3-azure-vm/main.tf
+++ 3-azure-vm/main.tf
@@ -20,3 +20,22 @@ resource "azurerm_virtual_network" "name" {
   location = "southindia"
   resource_group_name = "pradeep"
 }
+
+resource "azurerm_subnet" "subnet-1" {
+  name                 = "subnet-1"
+  resource_group_name  = "pradeep"
+  virtual_network_name = azurerm_virtual_network.name.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+resource "azurerm_subnet" "subnet-2" {
+  name                 = "subnet-2"
+  resource_group_name  = "pradeep"
+  virtual_network_name = azurerm_virtual_network.name.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+resource "azurerm_subnet" "subnet-3" {
+  name                 = "subnet-3"
+  resource_group_name  = "pradeep"
+  virtual_network_name = azurerm_virtual_network.name.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
