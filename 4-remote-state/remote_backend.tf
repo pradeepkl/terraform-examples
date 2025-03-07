16de7e7125e756478ea6f6afc4f7404ea1337c46
Pradeep <pradeep.kumar44@gmail.com>
2025-03-07 15:12:30 +0530

    Update remote_backend.tf

--- 4-remote-state/remote_backend.tf
+++ 4-remote-state/remote_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name = "pradeep"
-    storage_account_name = "terraformstate9qi5qj9d"
+    resource_group_name = "azure-devops"
+    storage_account_name = "terraformstate9qi5qj9dzzzddfdsf"
     container_name = "tfstate"
     key = "terraform.tfstate"
   }
