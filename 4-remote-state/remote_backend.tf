3788fca04e5627037e9c0ceebab9aada766cedc0
Pradeep <pradeep.kumar44@gmail.com>
2025-03-07 16:04:43 +0530

    Update remote_backend.tf

--- 4-remote-state/remote_backend.tf
+++ 4-remote-state/remote_backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "azurerm" {
+    subscription_id    = "f664773f-4f0b-429a-85af-b69d2b6e7100"
     resource_group_name = "azure-devops"
     storage_account_name = "tfstate07032025"
     container_name = "tfstate"
